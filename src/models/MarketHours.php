98714f4c4b7d68dc2f4b304aee28a8b0cfc5001a
Michael Pierre <mikepierredotcom@gmail.com>
2019-05-22 15:57:19 -0400

    first commit

---
+++ src/models/MarketHours.php
@@ -0,0 +1,23 @@
+<?php
+namespace TDAmeritradeSdk\models;
+
+use TDAmeritradeSdk\utils\CurlRequest;
+
+/**
+* 
+*/
+class MarketHours extends CurlRequest
+{
+	public function get(array $data)
+	{
+		$result =
+		$CurlRequest = new CurlRequest([
+			'TOKEN'=>
+			'END_POINT'=>,
+			'PARAMS'=>[]
+			]);
+
+		return $result;
+	}
+}
+?>
